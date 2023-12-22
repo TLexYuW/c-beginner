3ecbca3522cf09cd948b1c3390f1b1afc8abe422
LexYu <84486286+TLexYuW@users.noreply.github.com>
2023-12-22 18:38:25 +0800

    update pre-work

---
+++ macros/main.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+#define MAX 20
+
+#define PI 3.14
+
+#define print printf("PRINT THIS!\n");
+
+int main()
+{
+    printf("Max: %d\n", MAX);
+    
+    print
+
+    return 0;
+}
---
+++ preprocessor_directive/#line/main.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+
+int main()
+{
+    printf("error @ %s:%d\n", __FILE__, __LINE__);
+    
+    return 0;
+}
