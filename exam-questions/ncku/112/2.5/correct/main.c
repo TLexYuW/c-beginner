db3789dbcea77b481ff58e34412336c81d49e4c1
LexYu <84486286+TLexYuW@users.noreply.github.com>
2024-08-14 23:09:05 +0800

    remove

--- exam-questions/ncku/112/2.5/correct/main.c
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
-
-int i = 0;
-
-int main(int argc, char const *argv[])
-{
-    foo();
-    return 0;
-}
--- exam-questions/ncku/112/2.5/origin/foo.c
+++
@@ -1,7 +0,0 @@
-#include <stdio.h>
-
-int i = 123456;
-void foo()
-{
-    printf("%d", i);
-}
